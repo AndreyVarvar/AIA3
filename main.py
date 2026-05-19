bd6dd7a257de3d771aa2134bc93ae19e9e5daa69
Andrey Varvaryuk <andrey.varvaryuk@gmail.com>
2026-05-19 09:41:43 +0300

    fixed a bug

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ class Car:
         self.deceleration_speed = -2000
 
         self.rotation = rotation
-        self.rotation_speed = 1.5
+        self.rotation_speed = 3
 
         self.image = pg.Surface((100, 50), pg.SRCALPHA)
         pg.draw.rect(self.image, (255, 255, 255), (0, 0, 100, 50), 2)
@@ -42,6 +42,11 @@ class Car:
 
         for dest in self.destinations:
             pg.draw.circle(surf, (255, 255, 0), dest, 5)
+        
+        for r in [0, -pi/8, -pi/4]:
+            for i in range(1, 20):
+                p = self.pos + pg.Vector2(cos(self.rotation + r), -sin(self.rotation + r)) * 10 * i
+                pg.draw.circle(surf, (255, 0, 0), p, 5)
 
     def update(self, dt: float, signal: str, other_cars: list[Car]):
         # check if we can move
@@ -59,7 +64,7 @@ class Car:
         for car in other_cars:
             if car is not self:  
                 for r in [0, -pi/8, -pi/4]:
-                    for i in range(1, 10):
+                    for i in range(1, 20):
                         p = self.pos + pg.Vector2(cos(self.rotation + r), -sin(self.rotation + r)) * 10 * i
                         if dist(p, car.pos) < 50:
                             max_speed = 0
@@ -68,6 +73,11 @@ class Car:
                         break
             if max_speed == 0:
                 break
+
+        destination = self.destinations[0]
+        
+        if len(self.destinations) == 1:  # final destination
+            max_speed = min(dist(self.pos, destination)+100, max_speed)
         # check if we reached the speed limit
         if self.speed > max_speed:
             self.acceleration = self.deceleration_speed
@@ -80,20 +90,19 @@ class Car:
         self.speed /= 2**dt
 
         # turning
-        destination = self.destinations[0]
 
         angle = atan2(-destination.y + self.pos.y, destination.x - self.pos.x)
         if angle < 0:
             angle += 2*pi
-        if abs(angle - self.rotation) > 0.1:
+        if abs(angle - self.rotation) > 0.01:
+            diff = (angle - self.rotation) % (2*pi)
+            # print(f"{diff:.2f} {angle:.2f} {self.rotation:.2f}")
 
-            diff = (angle - self.rotation) % 2*pi
-            print(diff)
-            if diff < pi:
-                self.rotation += self.rotation_speed * dt * self.speed / self.speed_limit
+            rot = (self.rotation_speed - self.speed / self.speed_limit) * dt * self.speed / self.speed_limit
+            if 0 < diff < pi:
+                self.rotation += rot
             else:
-                self.rotation -= self.rotation_speed * dt * self.speed / self.speed_limit
-
+                self.rotation -= rot
         # applying the foces
         self.speed += self.acceleration * dt
         self.pos += pg.Vector2(cos(self.rotation), -sin(self.rotation)) * self.speed * dt
@@ -107,10 +116,10 @@ class Car:
 
 
 cars = [
-    Car(pg.Vector2(100, 100), [pg.Vector2(600, 100), pg.Vector2(800, 200)]),
-    # Car(pg.Vector2(100, 200), [pg.Vector2(600, 200)]),
-    # Car(pg.Vector2(100, 500), [pg.Vector2(600, 500)]),
-    # Car(pg.Vector2(400, 500), [pg.Vector2(400, 100)], rotation=pi/2),
+    Car(pg.Vector2(100, 100), [pg.Vector2(600, 100), pg.Vector2(800, 300)]),
+    Car(pg.Vector2(100, 200), [pg.Vector2(600, 200)]),
+    Car(pg.Vector2(100, 500), [pg.Vector2(600, 500)]),
+    Car(pg.Vector2(400, 500), [pg.Vector2(400, 100)], rotation=pi/2),
 ]
 
 
